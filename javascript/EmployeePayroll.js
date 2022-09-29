120b3b6bac4f941f589063b3ad81113dd31bfca3
Veer1498 <singavarapuveerendra@gmail.com>
2022-09-30 01:24:51 +0530

    [Veer] Added : Ability to Use Getters and Setters and Default Constructor

--- javascript/EmployeePayroll.js
+++ javascript/EmployeePayroll.js
@@ -1,20 +1,26 @@
 class EmployeePayrollData{
     // getter and setter method 
 
+    constructor (...param){
+        this._name = param[0]
+        this._profilePicture = param[1]
+        this._gender = param[2]
+        this._department = param[3]
+        this._salary = param[4]
+        this._startDate = param[5]
+        this._notes = param[6]
+    }
     get name() {
         return this._name;
     }
     set name(name) {
-        let nameRgx = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
-        if (nameRgx.test(name))
             this._name = name;
-        else throw 'Name is Incorrect!';
     }
-    get profilePicture() {
+    get profilePic() {
         return this._profilePicture;
     }
-    set profilePicture(profilePicture) {
-        this._profilePicture = profilePicture;
+    set profilePic(profilePicture) {
+        this.profilePicture = profilePicture;
     }
 
     get gender() {
@@ -46,21 +52,109 @@ class EmployeePayrollData{
     }
     set startDate(startDate) {
         let now = new Date();
-        if (startDate > now) throw "Start Date is a Futute Date!"
-        let difference = Math.abs(now.getTime() - startDate.getTime());
-        if (difference / (1000 * 60 * 60 * 24) < 30) {
+        let difference = Math.round(now - startDate);
+        if (startDate > now){
+            throw "Start Date is a Futute Date!"
+        }
+        
+        else if (difference > 30) {
             throw "Start Date is beyond 30 days!"
         }
-        this._startDate = startDate;
+        else{ 
+            this._startDate = startDate.toLocaleDateString('en-US');
+        }
+    }
+    get notes(){
+        return this._notes
+    }
+    set notes(notes){
+        this._notes = notes
     }
     
     toString() {
-        const options = { year: 'numeric', month: 'long', day: 'numeric' };
-        const empDate = !this.startDate ? "undefined" :
-            this.startDate.toLocaleDateString("en-US", options);
-
-        return "name='" + this.name + ", gender=" + this.gender +
-            ", profilePic=" + this.profilePic + ", department=" + this.department +
-            ",salary=" + this.salary + ", startDate=" + empDate + ",note=" + this.notes;
+        // if(this._startDate != empDate){
+        //     empDate = "undefined"
+        // }
+        // else{
+        //     empDate = this._startDate.toLocalDateString("en-US",{
+        //         year:'numeric',month:'long',day:'numeric'
+        //     });
+        // }
+        return "name='" + this.name +
+            ", profilePic=" + this.profilePic+", gender=" + this.gender + ",\n department=" + this.department +
+            ",salary=" + this.salary + ", startDate=" + this.startDate + ",note=" + this.notes;
     }
 }
+
+function save(){
+
+    //name
+   let empName = document.querySelector("#name");
+   empNam = empName.value
+
+    //profile pic
+   let empProfile = document.querySelectorAll('input[name="profile"]')
+    let empImage;
+   for(let image of empProfile){
+        if(image.checked){
+            empImage = image.value;
+            break;
+        }
+   }
+   //gender
+   let empGenders = document.querySelectorAll('input[name="gender"]')
+   let empGender;
+   for(let gender of empGenders){
+    if(gender.checked){
+        empGender = gender.value;
+        break;
+    }
+   }
+    
+   //Dept
+   let empDept = document.querySelectorAll('input[name="department"]')
+   let empDepartment = [];
+   for(let dept of empDept){
+    if(dept.checked){
+        empDepartment.push(dept.value);
+    }
+   }
+
+   //Salary
+   let empSalary = document.querySelector('input[name="salary"]')
+   let empSal = empSalary.value;
+
+
+   //Start Date   
+
+   let empStartDay = document.querySelector("#date #day option:checked")
+   let empDay = empStartDay.value;
+   let empStartMonth = document.querySelector("#month option:checked")
+   let empMonth = empStartMonth.value;
+   let empStartYear = document.querySelector("#year option:checked")
+   let empYear = empStartYear.value;
+   let empDate = Date.parse(empDay+" "+empMonth+" "+empYear)
+
+   //Notes
+   let empNotes = document.querySelector('#notes')
+   let empNote = empNotes.value;
+// Creating Employee Object
+   let empPayrollDataObject = new EmployeePayrollData(empNam,empImage,empGender,empDepartment,empSal,empDate,empNote)
+
+   saveToLocalStorage(empPayrollDataObject)
+}
+
+function saveToLocalStorage(empPayrollDataObject){
+    let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
+    if(employeePayrollList != undefined){
+        employeePayrollList.push(empPayrollDataObject);
+    }
+    else{
+        employeePayrollList = [empPayrollDataObject]
+    }
+    alert(employeePayrollList.toString());
+    localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList))
+}
+
+
+
